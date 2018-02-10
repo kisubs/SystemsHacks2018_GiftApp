a83652c9bc67c24db6f7a4c39724ff536f951915
mla189 <mla189@sfu.ca>
2018-02-10 11:51:06 -0800

    Link buttons on main activity

--- app/src/main/java/com/gazua/giftapp/MainActivity.java
+++ app/src/main/java/com/gazua/giftapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.gazua.giftapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +25,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e("main", "fuk");
         testCall();
+
+        Button btn_search = (Button) findViewById(R.id.btn_search);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Search Menu", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Button btn_best_seller = (Button) findViewById(R.id.btn_best_seller);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Best Seller", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Button btn_random = (Button) findViewById(R.id.btn_random);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Random", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Button btn_about = (Button) findViewById(R.id.btn_about);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "GAZUUUUUA!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
     }
+
+
 
     public void testCall() {
         SignedRequestsHelper helper;
--- app/src/main/java/com/gazua/giftapp/SplashActivity.java
+++ app/src/main/java/com/gazua/giftapp/SplashActivity.java
@@ -1,13 +1,15 @@
 package com.gazua.giftapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 5000;
 
@@ -18,11 +20,15 @@ public class Main2Activity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_splash);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Welcome!", Toast.LENGTH_SHORT);
+        toast.show();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
