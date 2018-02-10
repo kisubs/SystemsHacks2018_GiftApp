9adcbb3956664739add2e1304ab08f21f9b3e244
Kisub Song <ksong@velometro.com>
2018-02-10 11:48:08 -0800

    add AsyncTask to make url request

--- app/src/main/java/com/gazua/giftapp/MainActivity.java
+++ app/src/main/java/com/gazua/giftapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.gazua.giftapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.w3c.dom.Document;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static String request = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +40,9 @@ public class MainActivity extends AppCompatActivity {
         testCall();
     }
 
-    public void testCall() {
+
+
+    private void testCall() {
         SignedRequestsHelper helper;
 
         try {
@@ -40,11 +65,141 @@ public class MainActivity extends AppCompatActivity {
         params.put("AssociateTag", "giftapp69-20");
         params.put("SearchIndex", "All");
         params.put("ResponseGroup", "Images,ItemAttributes,Offers");
+        params.put("Keywords", "tablet");
 
         //parse this fucker
         requestUrl = helper.sign(params);
+        String decoded = "";
+        try {
+            decoded = URLDecoder.decode(requestUrl, "UTF-8");
+
+        } catch(Exception e) {
+
+        }
 
         Log.d("Main", "Signed URL: \"" + requestUrl + "\"");
+//        Log.d("Main", "URL decoded: \"" + getUrlContents(requestUrl) + "\"");
+
+//        request = requestUrl;
+
+
+        HttpAsyncTask task = new HttpAsyncTask();
+        task.execute(requestUrl);
+
+
+
 
     }
+
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String jsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL(params[0]);
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                jsonStr = buffer.toString();
+                return jsonStr;
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.i("json", s);
+        }
+    }
+
+
+    /*
+    private String getUrlContents(String theUrl)
+    {
+        StringBuilder content = new StringBuilder();
+
+        // many of these calls can throw exceptions, so i've just
+        // wrapped them all in one try/catch statement.
+        try
+        {
+            // create a url object
+            URL url = new URL(theUrl);
+
+            // create a urlconnection object
+            URLConnection urlConnection = url.openConnection();
+
+            // wrap the urlconnection in a bufferedreader
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+
+            // read from the urlconnection via the bufferedreader
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+    */
+
+
+
 }
