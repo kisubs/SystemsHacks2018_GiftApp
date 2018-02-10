49e74a50eff30c83afb554409d6b01e87bd945d5
Kisub Song <ksong@velometro.com>
2018-02-10 10:26:00 -0800

    add SignedRequestsHelper class

---
+++ app/src/main/java/com/gazua/giftapp/Constants.java
@@ -0,0 +1,11 @@
+package com.gazua.giftapp;
+
+/**
+ * Created by kisubs on 2018-02-10.
+ */
+
+public class Constants {
+    public static String endpoint = "webservices.amazon.ca"; // must be lowercase
+    public static String awsAccessKeyId = "AKIAIBWVJ4ZESU4AU22Q";
+    public static String awsSecretKey = "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";
+}
--- app/src/main/java/com/gazua/giftapp/MainActivity.java
+++ app/src/main/java/com/gazua/giftapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.gazua.giftapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("main", "fuk");
+        testCall();
+    }
+
+    public void testCall() {
+        SignedRequestsHelper helper;
+
+        try {
+            helper = SignedRequestsHelper.getInstance(
+                    Constants.endpoint,
+                    Constants.awsAccessKeyId,
+                    Constants.awsSecretKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        String requestUrl;
+
+        Map<String, String> params = new HashMap<>();
+
+        params.put("Service", "AWSECommerceService");
+        params.put("Operation", "ItemSearch");
+        params.put("AWSAccessKeyId", "AKIAIBWVJ4ZESU4AU22Q");
+        params.put("AssociateTag", "giftapp69-20");
+        params.put("SearchIndex", "All");
+        params.put("ResponseGroup", "Images,ItemAttributes,Offers");
+
+        //parse this fucker
+        requestUrl = helper.sign(params);
+
+        Log.d("Main", "Signed URL: \"" + requestUrl + "\"");
+
     }
 }
