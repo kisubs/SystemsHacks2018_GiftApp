b8d053a3b4c2c5642cb511359d8b2c9117da2a52
mla189 <mla189@sfu.ca>
2018-02-10 11:17:07 -0800

    Add splash, and main screen with options

--- app/src/main/java/com/gazua/giftapp/MainActivity.java
+++ app/src/main/java/com/gazua/giftapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.gazua.giftapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
         Log.e("main", "fuk");
         testCall();
